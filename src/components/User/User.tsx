5595fba4363b09d6250bb09650d4e7886593bc64
kaung222 <thirdgodiswinning@gmail.com>
2025-05-08 10:59:04 +0630

    add user filter

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -15,12 +15,21 @@ import { useGetUser } from "@/api/user/get-users";
 import { useDeleteUser } from "@/api/user/delete-user";
 import { useUpdateUser } from "@/api/user/update-user";
 import { useGerBraches } from "@/api/branch/branch.query";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const Users = () => {
   const { openDialog } = useDialogStore();
   const { data: users, isLoading, isError, error } = useGetUser();
   const { data: shops } = useGerBraches();
   const [dialogKey, setDialogKey] = useState("");
+  const [role, setRole] = useState("all");
+  const [branch, setBranch] = useState("all");
   const form = useForm<UserFormData>({
     resolver: zodResolver(UserSchema),
     defaultValues: {
@@ -28,6 +37,23 @@ const Users = () => {
       phone: "",
     },
   });
+
+  const filterUsers = () => {
+    if (role === "all" && branch === "all") {
+      return users;
+    }
+    if (role === "all" && branch !== "all") {
+      return users.filter((user) => user.branchId === parseInt(branch));
+    }
+    if (role !== "all" && branch === "all") {
+      return users.filter((user) => user.role === role);
+    }
+    if (role !== "all" && branch !== "all") {
+      return users.filter(
+        (user) => user.role === role && user.branchId === parseInt(branch)
+      );
+    }
+  };
 
   const onSubmit = async (data: UserFormData) => {
     console.log(data);
@@ -102,17 +128,47 @@ const Users = () => {
   return (
     <div className="mx-auto p-8">
       {/* User Form */}
-      <Button
-        variant="outline"
-        onClick={() => {
-          setDialogKey(dialogKeys.addNewUser);
-          openDialog(dialogKeys.addNewUser);
-          form.reset();
-        }}
-      >
-        <PlusCircleIcon />
-        Add new user
-      </Button>
+
+      <div className="flex gap-3">
+        <Button
+          variant="outline"
+          onClick={() => {
+            setDialogKey(dialogKeys.addNewUser);
+            openDialog(dialogKeys.addNewUser);
+            form.reset();
+          }}
+        >
+          <PlusCircleIcon />
+          Add new user
+        </Button>
+        <Select value={role} onValueChange={setRole}>
+          <SelectTrigger className="w-[180px] rounded-lg border-gray-300 shadow-sm">
+            <SelectValue placeholder="Filter" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">All</SelectItem>
+            <SelectItem value="admin">Admins</SelectItem>
+            <SelectItem value="technician">Technicians</SelectItem>
+            <SelectItem value="reception">Receptions</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Select value={branch} onValueChange={setBranch}>
+          <SelectTrigger className="w-[180px] rounded-lg border-gray-300 shadow-sm">
+            <SelectValue placeholder="Filter" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">All</SelectItem>
+            {shops.map((shop) => {
+              return (
+                <SelectItem key={shop.id} value={shop.id.toString()}>
+                  {shop.name}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
+      </div>
 
       <div className="mt-5">
         {isLoading ? (
@@ -121,7 +177,7 @@ const Users = () => {
           <p>Error: {error.message}</p>
         ) : users && users.length > 0 ? (
           <VirtualizedTable
-            data={users}
+            data={filterUsers()}
             columns={columns}
             rowKey={(user) => user.id}
             onRowClick={(user) => console.log("Row clicked:", user)}
