54a520cd8b75d7f772707dcccaf802ab1a591705
kaung222 <thirdgodiswinning@gmail.com>
2025-08-01 15:08:24 +0630

    fix error

--- src/api/service/service.mutation.ts
+++ src/api/service/service.mutation.ts
@@ -59,6 +59,7 @@ export const useUpdateService = (id: number) => {
       await api
         .patch(`/services/` + id, {
           ...data,
+          retrieveDate: data.retrievedDate,
         })
         .then((res) => res.data),
     onSuccess: async () => {
