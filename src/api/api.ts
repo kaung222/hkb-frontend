c86f0e509deac476d6422d3019c2ef4da39b4805
James <thirdgodiswinning@gmail.com>
2026-04-19 21:08:41 +0630

    change discount to 10

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_URL;
-export const defaultDiscount = 0;
+export const defaultDiscount = 10;
 
 const api = axios.create({
   baseURL: `${baseURL}/api/v1`,
